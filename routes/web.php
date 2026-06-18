8bb557710f98f7b2413e73ec5623e050d5049a6d
Mostafa <mishasoftgroup@gmail.com>
2026-06-18 22:21:04 +0330

    Some changes

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,8 @@
 <?php
 
-use App\Domains\Order\Actions\CreateOrderAction;
-use App\Domains\Order\Forward\Actions\ForwardOrderAction;
-use App\Domains\Order\Models\Order;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    //return view('telegram');
-    $action = new CreateOrderAction();
-
-    return $action->execute([
-        'project_id' => 1,
-        'product_id' => 1,
-        'quantity' => 2,
-        'status' => 'draft'
-    ]);
+    return view('telegram');
 });
 
-Route::get('/test-order', function () {
-
-    $action = new CreateOrderAction();
-
-    return $action->execute([
-        'project_id' => 1,
-        'product_id' => 2,
-        'quantity' => 3,
-        'description' => 'Test order from ERP system'
-    ]);
-});
-
-Route::get('/test-forward', function () {
-
-    $order = Order::first();
-
-    $action = new ForwardOrderAction();
-
-    return $action->execute($order, 2); // department_id = 2
-});
